49ea4901932a30a30d2016eee7f779d185e28502
YtangEMB <ytangchelo22@gmail.com>
2024-11-26 21:36:31 -0600

    Api de sucursales completas

--- src/java/org/utl/dsm/model/Sucursal.java
+++ src/java/org/utl/dsm/model/Sucursal.java
@@ -3,129 +3,93 @@ package org.utl.dsm.model;
 public class Sucursal {
     private int idSucursal;
     private String nombre;
-    private String latitud;
-    private String longitud;
+    private double latitud;
+    private double longitud;
     private String foto;
     private String urlWeb;
     private String horarios;
-    private String calle;
-    private String numCalle;
-    private String colonia;
-    private int idCiudad;
-    private int activo;
+    private String direccion;
+    private String ciudadNombre;
+    private String estadoNombre;
 
-    public Sucursal() {
-    }
-
-    public Sucursal(int idSucursal, String nombre, String latitud, String longitud, String foto, String urlWeb, String horarios, String calle, String numCalle, String colonia, int idCiudad, int activo) {
-        this.idSucursal = idSucursal;
-        this.nombre = nombre;
-        this.latitud = latitud;
-        this.longitud = longitud;
-        this.foto = foto;
-        this.urlWeb = urlWeb;
-        this.horarios = horarios;
-        this.calle = calle;
-        this.numCalle = numCalle;
-        this.colonia = colonia;
-        this.idCiudad = idCiudad;
-        this.activo = activo;
-    }
-
+    // Getters y Setters
     public int getIdSucursal() {
         return idSucursal;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getLatitud() {
-        return latitud;
-    }
-
-    public String getLongitud() {
-        return longitud;
-    }
-
-    public String getFoto() {
-        return foto;
-    }
-
-    public String getUrlWeb() {
-        return urlWeb;
-    }
-
-    public String getHorarios() {
-        return horarios;
-    }
-
-    public String getCalle() {
-        return calle;
-    }
-
-    public String getNumCalle() {
-        return numCalle;
-    }
-
-    public String getColonia() {
-        return colonia;
-    }
-
-    public int getIdCiudad() {
-        return idCiudad;
-    }
-
-    public int getActivo() {
-        return activo;
     }
 
     public void setIdSucursal(int idSucursal) {
         this.idSucursal = idSucursal;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public void setLatitud(String latitud) {
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
         this.latitud = latitud;
     }
 
-    public void setLongitud(String longitud) {
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
         this.longitud = longitud;
+    }
+
+    public String getFoto() {
+        return foto;
     }
 
     public void setFoto(String foto) {
         this.foto = foto;
     }
 
+    public String getUrlWeb() {
+        return urlWeb;
+    }
+
     public void setUrlWeb(String urlWeb) {
         this.urlWeb = urlWeb;
+    }
+
+    public String getHorarios() {
+        return horarios;
     }
 
     public void setHorarios(String horarios) {
         this.horarios = horarios;
     }
 
-    public void setCalle(String calle) {
-        this.calle = calle;
+    public String getDireccion() {
+        return direccion;
     }
 
-    public void setNumCalle(String numCalle) {
-        this.numCalle = numCalle;
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 
-    public void setColonia(String colonia) {
-        this.colonia = colonia;
+    public String getCiudadNombre() {
+        return ciudadNombre;
     }
 
-    public void setIdCiudad(int idCiudad) {
-        this.idCiudad = idCiudad;
+    public void setCiudadNombre(String ciudadNombre) {
+        this.ciudadNombre = ciudadNombre;
     }
 
-    public void setActivo(int activo) {
-        this.activo = activo;
+    public String getEstadoNombre() {
+        return estadoNombre;
+    }
+
+    public void setEstadoNombre(String estadoNombre) {
+        this.estadoNombre = estadoNombre;
     }
 }
-
